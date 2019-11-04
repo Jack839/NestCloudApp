ed65c30604d31f8684a013e0607bd9d5008a5769
Ninad Sutrave <ninadsutrave@gmail.com>
2019-11-05 02:13:27 +0530

    added additional functionality
    Added the Logo
    Added the App Title
    Added the Button

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,87 +1,386 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
 
 
-void main() {
-  
-    runApp(
-    MyApp()
-  );
-}
-class MyApp extends StatefulWidget{
-  @override
-  State<StatefulWidget> createState(){
-    return MyAppState();
-  }
-}
+class MyApp extends StatelessWidget {
 
-class MyAppState extends State<MyApp> {
-  int _selectedPage = 0;
-  final _pageOptions = [
-    Text('Item 1'),
-    Text('Item 2'),
-    Text('Item 3'),
-  ];
   @override
   Widget build(BuildContext context) {
-    return 
-      MaterialApp(
-        theme: ThemeData(
+
+    return MaterialApp( 
+
+      theme: ThemeData(
           primarySwatch: Colors.red,
         ) ,
-        home:Scaffold(
+
+      home: Scaffold(
+
+        body: SafeArea( child: _createStack(), ),
+               
+       
+      )
+    );     
+             
+  }
+
+  _createStack() {
+    return new Stack(
+        children: <Widget>[
+
+            new Align(
+
+            alignment: new Alignment(0.0, 0.0),
+            child: Container(
+
+              child: MyApp2(),
+
+            ),
+
+            ),
+
+            new Align(
+
+            alignment: const Alignment(-0.95, -1.25),
+
+            child: new Container( 
+
+              height: 200.0,
+              width: 150.0,
+
+              constraints: BoxConstraints(
+                maxHeight: 200.0,
+                maxWidth: 150.0,
+              ),
+              
+              child: Image.asset('assets/images/logo_black_transparent.png'),
+
+            )
+          ),
+
           
-          body: SafeArea(
+
+          new Align(
+            alignment: new Alignment(-0.92, -0.87),
+
+            child: Container(
+                  decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blueAccent)
+                          ),
+                 child: new Text(
+                'NestCloudApp' ,style: new TextStyle(color: Colors.black, fontSize: 23.0, fontWeight: FontWeight.bold))
+
+            ),
+          ),
+
+          new Align(
+
+            alignment: new Alignment(-0.90, -0.80),
+
+            child: Container (child: Divider()),
+
+          ),
+
+
+
+          new Align(
+
+            alignment: new Alignment(1.0, -1.0),
+            child: RaisedButton(
+              
+              child: Text('CLICK'),
+
+              onPressed: () {}
+                
+              )
+
+          
+            ),
+          
+
+          new Align(
             
-            child:Container(
-            margin: EdgeInsets.only(top:120,bottom: 20,right: 20,left: 20),
-            child: DataTable(
+            alignment: new Alignment( -0.80, -0.80), 
+
+            child: new Container (
+
+                        
+            margin: EdgeInsets.only(top: 100, bottom: 60, right: 15, left: 15),
+            
+
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                  child: Container(
+                      height: 800.0,
+                      width: double.infinity,
+                      color: Colors.lightBlue[100],
+
+
+                       
+                      child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                      
+                    child:  DataTable(
+
               columns: [
-                DataColumn(label: Text("Nigga")),
-                DataColumn(label: Text("Fuck"))
+
+                DataColumn(label: Text("Hello")),
+                DataColumn(label: Text("World")),
+                
+
               ],
               rows: [
-                DataRow(cells: [
-                  DataCell(Text('Nigger')),
-                  DataCell(Text('Bitch'))
-                ])
-              ],
-            ),
-            
-            width: 350,
-            color: Colors.cyan  ,
-          )
-            
-          ),
-          
-          bottomNavigationBar: BottomNavigationBar(
-            backgroundColor: Colors.blue,
-            currentIndex: _selectedPage,
 
-            onTap: (int index){ 
-            setState(() {
-              _selectedPage=index;
-            });
-            },
-            items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              title: Text('home')
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.hot_tub),
-              title: Text('hot tub')
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              title: Text('home')
-            ),
-            ]
-          ),
-        )
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                ),
+
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                ),
+
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                ),
+
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                ),
+
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                ),
+
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                ),
+
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                ),
+
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                ),
+
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                ),
+
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                ),
+
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                ),
+
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                ),
+
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                ),
+
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                ),
+
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                ),
+
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                ),
+
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                ),
+
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                ),
+
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                ),
+
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                ),
+
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                ),
+
+                DataRow(
+                                    
+                  cells: [
+                  DataCell(Text("Data")),
+                  DataCell(Text("Item")),
+                  
+                  ]
+                )
+
+                      
+                
+
+              ],
+
+              ))
+                  )
+
+
+        ),
+        ),
+    ),
+          ),          
+        ]   
     );
-    
+  }
+
+}
+
+class MyApp2 extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState(){
+    return MyApp2State();
   }
 }
 
+class MyApp2State extends State<MyApp2> {
 
+  int _index = 0;
+
+    @override
+    Widget build(BuildContext context) {
+
+       return Scaffold(
+
+             bottomNavigationBar: BottomNavigationBar(
+               
+                    backgroundColor: Colors.lightBlueAccent[100],
+                    onTap: (newIndex) => setState(() => _index = newIndex),
+                    currentIndex: _index,
+                    
+                    items: [
+                      BottomNavigationBarItem(icon: Icon(Icons.looks_one), title: Text("Screen1")),
+                      BottomNavigationBarItem(icon: Icon(Icons.looks_two), title: Text("Screen2")),
+                      BottomNavigationBarItem(icon: Icon(Icons.looks_3), title: Text("Screen3")),
+                    ],
+         ),
+     );
+  }
+
+
+
+}
